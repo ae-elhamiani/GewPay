2b1d0a4a733c05bca4ec40f64112c26d5c921d83
ab-ben <abdssamadbendahman@gmail.com>
2024-08-19 14:26:26 +0100

    set up plugin with storeId and ApiKey validation + order data fetching

--- server/payment-service/src/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const paymentController = require('../controllers/paymentController');
-
-const router = express.Router();
-
-router.post('/create-payment', paymentController.createPayment);
-router.post('/payment-webhook', paymentController.updatePaymentStatus);
-
-module.exports = router;
